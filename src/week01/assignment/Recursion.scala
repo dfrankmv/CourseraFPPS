03ee5f2188fe56f3bb6019ca959bd7dd2eeec04d
dfrankmv <dfrankmv@gmail.com>
2016-06-11 15:29:27 -0500

    Week 01 - Assignment - Start

---
+++ src/week01/assignment/Recursion.scala
@@ -0,0 +1,27 @@
+package week01.assignment
+
+object Recursion {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = ???
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
