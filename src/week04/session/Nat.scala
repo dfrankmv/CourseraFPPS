0d6136b4510b8ec5c9e8da38e0938f2264fac5f1
dfrankmv <dfrankmv@gmail.com>
2016-06-14 07:07:56 -0500

    Lecture 4.2 - Objects Everywhere

---
+++ src/week04/session/Nat.scala
@@ -0,0 +1,30 @@
+package week04.session
+
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor = new Succ(this)
+  def +(that: Nat): Nat
+  def -(that: Nat): Nat
+}
+
+object Zero extends Nat {
+  override def isZero: Boolean = true
+
+  override def +(that: Nat): Nat = that
+
+  override def -(that: Nat): Nat = if (that.isZero) this else throw new Error("negative number")
+
+  override def predecessor: Nat = throw new Error("0.predecessor")
+}
+
+class Succ(n: Nat) extends Nat {
+  override def isZero: Boolean = false
+
+  override def +(that: Nat): Nat = new Succ(n + that)
+
+  override def -(that: Nat): Nat = if (that.isZero) this else n - that.predecessor
+
+  override def predecessor: Nat = n
+}
+
